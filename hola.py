40af4c3ac7b349616c406c9bc73076e1de88801d
Kiara2106 <a.kiarasoto@icea.edu.ar>
2024-10-08 18:08:05 +0000

    h

--- hola.py
+++ hola.py
@@ -1,1 +1,15 @@
-hola
+hola=1
+hola=True
+hola=False
+hola='hola'
+hola="hola"
+print(hola)
+for x in range(20):
+    print(x)
+if 1+1==2:
+    print(True)
+while False:
+    print(0)
+valor='kiara'
+kiara="."
+nombre=''
